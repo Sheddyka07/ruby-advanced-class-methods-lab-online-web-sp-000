b6c054a80a8d7e9a256717684bf7e12411b47b84
Kelly Sheddy <jungaka07@gmail.com>
2018-11-29 14:29:04 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,13 +1,38 @@
 class Song
   attr_accessor :name, :artist_name
   @@all = []
-
+ 
   def self.all
     @@all
   end
-
+  
   def save
     self.class.all << self
   end
-
+  
+  def self.create
+    song = self.new
+    song.save
+    song
+  end
+ 
+  def self.new_by_name(song_name)
+    song = self.new
+    song.name = song_name
+    song 
+  end
+  
+  def self.create_by_name(song_name)
+    song = self.new
+    song.name = song_name
+    song.save
+    song
+  end
+  
+  def self.find_by_name(song_name)
+    
+    
+    
+  
+ 
 end
